6ff80ae0ce39b4a3c43335bcbd6719387ecf5f0e
EddyGlezz <eddyglezz95@gmail.com>
2023-01-17 11:21:24 +0100

    use a cdn for javascript

--- wasm/AfterInitFunctions.js
+++ wasm/AfterInitFunctions.js
@@ -1,9 +1,9 @@
 //<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.14.0/ace.js" integrity="sha512-WYlXqL7GPpZL2ImDErTX0RMKy5hR17vGW5yY04p9Z+YhYFJcUUFRT31N29euNB4sLNNf/s0XQXZfzg3uKSoOdA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script type="text/javascript" src="https://raw.githubusercontent.com/EddyTheCo/qmlonline/main/wasm/js/ace.js"></script>
-<script type="text/javascript" src="https://raw.githubusercontent.com/EddyTheCo/qmlonline/main/wasm/js/ext-beautify.js"></script>
-<script type="text/javascript" src="https://raw.githubusercontent.com/EddyTheCo/qmlonline/main/wasm/js/ext-language_tools.js"></script>
-<script type="text/javascript" src="https://raw.githubusercontent.com/EddyTheCo/qmlonline/main/wasm/js/mode-qml.js"></script>
-<script type="text/javascript" src="https://raw.githubusercontent.com/EddyTheCo/qmlonline/main/wasm/js/theme-chaos.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/EddyTheCo/qmlonline@main/wasm/js/ace.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/EddyTheCo/qmlonline@main/wasm/js/ext-beautify.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/EddyTheCo/qmlonline@main/wasm/js/ext-language_tools.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/EddyTheCo/qmlonline@main/wasm/js/mode-qml.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/EddyTheCo/qmlonline@main/wasm/js/theme-chaos.js"></script>
 <script>
 
 qtLoader = initQTwasm('https://raw.githubusercontent.com/EddyTheCo/qmlonline/main/wasm/', 'appqmlonline', '#qtrootDiv', 'img/qtlogo.svg');
