ea38a7958b52493eb4f17fbc299459f57fc6e730
EddyGlezz <eddyglezz95@gmail.com>
2023-03-03 21:31:12 +0100

    removed the registering of the singleton on the main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include "qmltextcode.h"
-#include<iostream>
 #ifdef USE_QtQrGen
 #include "Qrimageprovider.hpp"
 #endif
@@ -10,24 +9,18 @@
 
 int main(int argc, char *argv[])
 {
-	qmlTextCode *Code = new qmlTextCode();
-	qmlRegisterSingletonType<qmlTextCode>("Textcode", 1, 0, "JCode",
-			[&Code](QQmlEngine *engine, QJSEngine *scriptEngine) -> QObject * {
-			Q_UNUSED(engine)
-			Q_UNUSED(scriptEngine)
-
-
-			return Code;
-			});
 	QGuiApplication app(argc, argv);
 
 	QQmlApplicationEngine engine;
+    engine.addImportPath("qrc:/esterVtech.com/imports");
+
 #ifdef USE_QtQrGen
     engine.addImageProvider(QLatin1String("qrCodedarkred"), new QRImageProvider("darkred",1));
     engine.addImageProvider(QLatin1String("qrCodeBlue"), new QRImageProvider("blue",1));
     engine.addImageProvider(QLatin1String("qrCodeBlack"), new QRImageProvider("black",1));
     engine.addImageProvider(QLatin1String("qrCodemaroon"), new QRImageProvider("maroon",1));
 #endif
+    qDebug()<<engine.importPathList();
 	const QUrl url(u"qrc:/qmlonline/main.qml"_qs);
 	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
 			&app, [url](QObject *obj, const QUrl &objUrl) {
