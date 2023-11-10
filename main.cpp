6f5dfcdb7b71ff9866b74187f3709d92e1836dbc
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-11-10 12:46:32 +0100

    Fix static linking (#24)
    * use lates dependencies
    
    * static link the plugin 
    
    * updated readme
    
    * use latest qt api

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 #ifdef USE_QtQr
 #include "Qrimageprovider.hpp"
-#include"Qrimagedecoder.hpp"
+#include "Qrimagedecoder.hpp"
 #endif
 
 
@@ -14,23 +14,18 @@ int main(int argc, char *argv[])
 	QGuiApplication app(argc, argv);
 
 	QQmlApplicationEngine engine;
-    engine.addImportPath("qrc:/esterVtech.com/imports");
+	engine.addImportPath("qrc:/esterVtech.com/imports");
 
 #ifdef USE_QtQr
-    engine.addImageProvider(QLatin1String("qrCodedarkred"), new QRImageProvider("darkred",1));
-    engine.addImageProvider(QLatin1String("qrCodeBlue"), new QRImageProvider("blue",1));
-    engine.addImageProvider(QLatin1String("qrCodeBlack"), new QRImageProvider("black",1));
-    engine.addImageProvider(QLatin1String("qrCodemaroon"), new QRImageProvider("maroon",1));
-    engine.addImageProvider(QLatin1String("wasm"), new WasmImageProvider());
+	engine.addImageProvider(QLatin1String("qrCodedarkred"), new QRImageProvider("darkred",1));
+	engine.addImageProvider(QLatin1String("qrCodeBlue"), new QRImageProvider("blue",1));
+	engine.addImageProvider(QLatin1String("qrCodeBlack"), new QRImageProvider("black",1));
+	engine.addImageProvider(QLatin1String("qrCodemaroon"), new QRImageProvider("maroon",1));
+	engine.addImageProvider(QLatin1String("wasm"), new WasmImageProvider());
 #endif
-    qDebug()<<engine.importPathList();
-    qmlRegisterSingletonType(QUrl(u"qrc:/esterVtech.com/imports/MyDesigns/qml/CustomStyle.qml"_qs), "CustomStyle", 1, 0, "CustomStyle");
+	qDebug()<<engine.importPathList();
+	qmlRegisterSingletonType(QUrl(u"qrc:/esterVtech.com/imports/MyDesigns/qml/CustomStyle.qml"_qs), "CustomStyle", 1, 0, "CustomStyle");
 	const QUrl url(u"qrc:/esterVtech.com/imports/qmlonline/main.qml"_qs);
-	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-			&app, [url](QObject *obj, const QUrl &objUrl) {
-			if (!obj && url == objUrl)
-			QCoreApplication::exit(-1);
-			}, Qt::QueuedConnection);
 	engine.load(url);
 
 	return app.exec();
