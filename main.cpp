d72200ab25bf02752bc1e6571e3a6bcbbe7475a6
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-12-09 14:10:25 +0100

    use the latest controls added the date time pickers and the theme swi… (#42) (#43)
    * use the latest controls added the date time pickers and the theme switching
    
    * added qt multimedia even if not used

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-
+#include <QQuickStyle>
 #ifdef USE_QtQr
 #include "Qrimageprovider.hpp"
 #include "Qrimagedecoder.hpp"
@@ -12,20 +12,16 @@
 int main(int argc, char *argv[])
 {
 	QGuiApplication app(argc, argv);
+    QQuickStyle::setStyle("Esterv.Controls.Flat");
 
 	QQmlApplicationEngine engine;
 	engine.addImportPath("qrc:/esterVtech.com/imports");
 
 #ifdef USE_QtQr
-	engine.addImageProvider(QLatin1String("qrCodedarkred"), new QRImageProvider("darkred",1));
-	engine.addImageProvider(QLatin1String("qrCodeBlue"), new QRImageProvider("blue",1));
-	engine.addImageProvider(QLatin1String("qrCodeBlack"), new QRImageProvider("black",1));
-	engine.addImageProvider(QLatin1String("qrCodemaroon"), new QRImageProvider("maroon",1));
+    engine.addImageProvider(QLatin1String("qrcode"), new QRImageProvider(1));
 	engine.addImageProvider(QLatin1String("wasm"), new WasmImageProvider());
 #endif
-	qDebug()<<engine.importPathList();
-	qmlRegisterSingletonType(QUrl(u"qrc:/esterVtech.com/imports/MyDesigns/qml/CustomStyle.qml"_qs), "CustomStyle", 1, 0, "CustomStyle");
-	const QUrl url(u"qrc:/esterVtech.com/imports/qmlonline/main.qml"_qs);
+    const QUrl url("qrc:/esterVtech.com/imports/qmlonline/main.qml");
 	engine.load(url);
 
 	return app.exec();
